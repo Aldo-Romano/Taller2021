6664a1d3e532909e7aefb77ec3362d23d44f71ff
Ceciah Ismari <ceciahisma@gmail.com>
2021-08-02 16:13:58 -0500

    06.Se agregaron las clases Revisiones y Mecanicos

---
+++ ClassEntidades/Mecanicos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class Mecanicos
+    {
+        public int id_Tecnico { get; set; }
+        public string Nombre { get; set; }
+        public string App { get; set; }
+        public string Apm { get; set; }
+        public string Celular { get; set; }
+        public string Correo { get; set; }
+    }
+}
---
+++ ClassEntidades/Revisiones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class Revisiones
+    {
+        public int id_Revision { get; set; }
+        public DateTime Entrada { get; set; }
+        public string Falla { get; set; }
+        public string diagnostico { get; set; }
+        public byte Autorizacion { get; set; }
+        public int  Auto { get; set; }
+        public int Mecanico { get; set; }
+
+    }
+}
