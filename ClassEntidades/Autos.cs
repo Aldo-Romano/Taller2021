6145d0927353cfaaa8aef501ed04ec21aab24236
Aldo Romano <aldoromanorojas@gmail.com>
2021-08-02 01:45:21 -0500

    04.Modificacion a clases Clientes y Autos

--- ClassEntidades/Autos.cs
+++ ClassEntidades/Autos.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ClassEntidades
 {
-    class Autos
+    public class Autos
     {
+        public int id_Auto { get; set; }
+        public int f_marca { get; set; }
+        public string modelo { get; set; }
+        public string año { get; set; }
+        public string color { get; set; }
+        public string placas { get; set; }
+        public int dueño { get; set; }
     }
 }
--- ClassEntidades/Clientes.cs
+++ ClassEntidades/Clientes.cs
@@ -8,5 +8,13 @@ namespace ClassEntidades
 {
     public class Clientes
     {
+        public int id_cliente { get; set; }
+        public string nombre { get; set; }
+        public string app { get; set; }
+        public string apm { get; set; }
+        public string celular { get; set; }
+        public string telOficina { get; set; }
+        public string correoPer { get; set; }
+        public string correoCorp { get; set; }
     }
 }
