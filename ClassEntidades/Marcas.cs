282ca60f73738c2c299fc849f2a6790e6e865020
Alison Aranza Tellez <aranzateca@gmail.com>
2021-08-02 02:01:18 -0500

    05.Modificación de clases Marcas-Reparacionesen capa Entidades

---
+++ ClassEntidades/Marcas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class Marcas
+    {
+        public int id_marca { get; set; }
+        public string marca { get; set; }
+    }
+}
---
+++ ClassEntidades/Reparaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class Reparaciones
+    {
+        public int id_reparacion { get; set; }
+        public string detalles { get; set; }
+        public string garantia { get; set; }
+        public DateTime salida { get; set; }
+        public int Fk_revision { get; set; }
+    }
+}
